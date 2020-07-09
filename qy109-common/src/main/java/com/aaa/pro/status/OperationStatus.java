53e080ed02097cd3110a32ae03065574ca0103d7
1755103832 <1755103832@qq.com>
2020-07-09 21:26:05 +0800

    7-9-9:25

--- qy109-common/src/main/java/com/aaa/pro/status/OperationStatus.java
+++ qy109-common/src/main/java/com/aaa/pro/status/OperationStatus.java
@@ -8,16 +8,11 @@ package com.aaa.pro.status;
 public enum OperationStatus {
 
     // 其他操作状态码
-    OPERATION_SUCCESS("1", "操作成功"),
-    OPERATION_FAILED("2", "操作失败"),
-    DELETE_OPERATION("3", "删除操作"),
-    UPDATE_OPERATION("4", "修改操作"),
-    INSERT_OPERATION("5", "新增操作"),
-    ZUUL_FILTER_SUCCESS("6", "路由过滤成功"),
-    ZUUL_FILTER_FAILED("7", "路由过滤失败"),
-    ZUUL_FILTER_TOKEN_SUCCESS("8", "token值存在"),
-    ZUUL_FILTER_TOKEN_FAILED("9", "token值不存在"),
-    REQUEST_IS_NULL("10", "request对象为null");
+    ZUUL_FILTER_SUCCESS("1", "路由过滤成功"),
+    ZUUL_FILTER_FAILED("2", "路由过滤失败"),
+    ZUUL_FILTER_TOKEN_SUCCESS("3", "token值存在"),
+    ZUUL_FILTER_TOKEN_FAILED("4", "token值不存在"),
+    REQUEST_IS_NULL("5", "request对象为null");
 
     private String code;
 
--- qy109-provider-management/backup-project-provider/src/main/java/com/aaa/pro/base/CommonController.java
+++ qy109-provider-management/backup-project-provider/src/main/java/com/aaa/pro/base/CommonController.java
@@ -2,8 +2,14 @@ package com.aaa.pro.base;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -78,9 +84,9 @@ public abstract class CommonController<T> extends BaseController {
         Integer addResult = getBaseService().insert(instance);
         if (addResult > 0) {
             afterAdd(map);
-            return super.operationSuccess();
+            return super.insertSuccess();
         }
-        return super.operationFailed();
+        return super.insertFailed();
     }
 
     /**
@@ -94,9 +100,9 @@ public abstract class CommonController<T> extends BaseController {
         T instance = getBaseService().newInstance(map);
         Integer deleteResult = getBaseService().deleteByPrimaryKey(instance);
         if (deleteResult > 0) {
-            return super.operationSuccess();
+            return super.deleteSuccess();
         }
-        return super.operationFailed();
+        return super.deleteFailed();
     }
 
     /**
@@ -109,8 +115,102 @@ public abstract class CommonController<T> extends BaseController {
     public ResultData batchDelete(@RequestParam("ids[]") Integer[] ids) {
         Integer deleteResult = getBaseService().batchDeleteByIds(Arrays.asList(ids));
         if (deleteResult > 0) {
-            return super.operationSuccess();
+            return super.deleteSuccess();
         }
-        return super.operationFailed();
+        return super.deleteFailed();
+    }
+
+    /**
+     * @Author zyb
+     * @Description 修改数据
+     * @Date 2020/7/9 17:22
+     * @Param [map]
+     * @Return com.aaa.pro.base.ResultData
+     **/
+    public ResultData updateByPrimaryKeySelective(@RequestBody Map map) {
+        T instance = getBaseService().newInstance(map);
+        Integer i = getBaseService().updateByPrimaryKeySelective(instance);
+        if (i > 0) {
+            return super.updateSuccess();
+        }
+        return super.updateFailed();
+    }
+
+//    public ResultData batchUpdate(@RequestParam("ids[]") Integer[] ids,T t){
+//        getBaseService().newInstance((Map) t)
+//        getBaseService().batchUpdate()
+//    }
+
+    /**
+     * @Author zyb
+     * @Description 查询一条数据
+     * @Date 2020/7/9 17:28
+     * @Param [map]
+     * @Return com.aaa.pro.base.ResultData
+     **/
+    public ResultData selectOne(@RequestBody Map map) {
+        T instance = getBaseService().newInstance(map);
+        T t = getBaseService().selectOne(instance);
+        if (null != t && !"".equals(t)) {
+            return super.querySuccess();
+        }
+        return super.queryFailed();
+    }
+
+    /**
+     * @Author zyb
+     * @Description 查询多条数据
+     * @Date 2020/7/9 21:17
+     * @Param [map]
+     * @Return com.aaa.pro.base.ResultData
+     **/
+    public ResultData selectList(@RequestBody Map map) {
+        T instance = getBaseService().newInstance(map);
+        List<T> list = getBaseService().selectList(instance);
+        if (null != list && !"".equals(list)) {
+            return super.querySuccess();
+        }
+        return super.queryFailed();
+    }
+
+    /**
+     * @Author zyb
+     * @Description 防止数据不安全，所以不能直接在controller某个方法中直接接收HttpServletRequest对象
+     * 必须要从本地当前线程中获取对象
+     * @Date 2020/7/9 21:18
+     * @Param []
+     * @Return javax.servlet.http.HttpServletRequest
+     **/
+    public HttpServletRequest getServletRequest() {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes servletRequestAttributes;
+        if (requestAttributes instanceof ServletRequestAttributes) {
+            servletRequestAttributes = (ServletRequestAttributes) requestAttributes;
+            return servletRequestAttributes.getRequest();
+        }
+        return null;
+    }
+
+    /**
+     * @Author zyb
+     * @Description 获取当前客户端的session对象(如果不存在 ， 则会重新创建一个)
+     * @Date 2020/7/9 21:18
+     * @Param []
+     * @Return javax.servlet.http.HttpSession
+     **/
+    public HttpSession getSession() {
+        return getServletRequest().getSession();
+    }
+
+    /**
+     * @Author zyb
+     * @Description 获取当前客户端的session对象(如果不存在 ， 则直接返回为null)
+     * @Date 2020/7/9 21:18
+     * @Param []
+     * @Return javax.servlet.http.HttpSession
+     **/
+    public HttpSession getExistSession() {
+        return getServletRequest().getSession(false);
+
     }
 }
