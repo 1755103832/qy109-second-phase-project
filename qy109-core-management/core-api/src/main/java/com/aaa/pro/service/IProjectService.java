fc41c7c4ac7648d2c9680c31a92d2c68c56db968
1755103832 <1755103832@qq.com>
2020-07-18 09:26:04 +0800

    7-18 9:25

--- qy109-core-management/core-api/src/main/java/com/aaa/pro/service/IProjectService.java
+++ qy109-core-management/core-api/src/main/java/com/aaa/pro/service/IProjectService.java
@@ -324,8 +324,7 @@ public interface IProjectService {
     List<Mapping_unit> queryMapping_unit(@RequestParam("userId") Long userId);
 
     /**
-     * @description:
-     *   测绘管理--单位基本信息--查询单位负责人信息
+     * @description: 测绘管理--单位基本信息--查询单位负责人信息
      * @params: [userId]
      * @return: java.util.List<com.aaa.pro.model.Principal>
      * @author: Wen
@@ -335,8 +334,7 @@ public interface IProjectService {
     List<Principal> queryOne(@RequestParam("userId") Long userId);
 
     /**
-     * @description:
-     *   测绘管理-单位基本信息--修改负责人信息
+     * @description: 测绘管理-单位基本信息--修改负责人信息
      * @params: [principal]
      * @return: java.lang.Boolean
      * @author: Wen
@@ -345,4 +343,69 @@ public interface IProjectService {
     @PostMapping("/updateList")
     Boolean updateList(@RequestBody Principal principal);
 
+    /**
+     * @Author zyb
+     * @Description 通过项目信息id编号查询项目审核记录
+     * @Date 2020/7/17 19:28
+     * @Param [id, pageNum, pageSize]
+     * @Return com.github.pagehelper.PageInfo<com.aaa.pro.model.Audit>
+     **/
+    @GetMapping("/selectAuditRecordByMappingProjectId")
+    PageInfo<Audit> selectAuditRecordByMappingProjectId(@RequestParam("id") Long id,
+                                                        @RequestParam("pageNum") Integer pageNum,
+                                                        @RequestParam("pageSize") Integer pageSize);
+
+    /**
+     * @return java.util.List<com.aaa.pro.model.Equipment>
+     * @Author: jkm
+     * @Description: 根据 userId 查询设备信息
+     * @Date: 19:30 2020/7/17
+     * @param: [userId]
+     */
+    @PostMapping("/selectAllEquipmentByUserId")
+    List<Equipment> selectAllEquipmentByUserId(@RequestParam("userId") Long userId);
+
+    /**
+     * @return com.github.pagehelper.PageInfo
+     * @Author: jkm
+     * @Description: 查询所有角色信息
+     * @Date: 18:12 2020/7/17
+     * @param: [pageNo, pageSize]
+     */
+    @GetMapping("/selectAllRole")
+    PageInfo selectAllRole(@RequestParam("pageNo") Integer pageNo, @RequestParam("pageSize") Integer pageSize);
+
+    /**
+     * @return com.github.pagehelper.PageInfo
+     * @Author: jkm
+     * @Description: 根据条件查询
+     * @Date: 18:11 2020/7/17
+     * @param: [map, pageNo, pageSize]
+     */
+    @PostMapping("/selectRoleByField")
+    PageInfo selectRoleByField(@RequestBody Map map,
+                               @RequestParam("pageNo") Integer pageNo,
+                               @RequestParam("pageSize") Integer pageSize);
+
+    /**
+     * @return com.aaa.pro.model.Role
+     * @Author: jkm
+     * @Description: 根据主键查询角色信息
+     * @Date: 18:11 2020/7/17
+     * @param: [roleId]
+     */
+    @GetMapping("selectRoleByPrimaryKey")
+    Role selectRoleByPrimaryKey(@RequestParam("roleId") Long roleId);
+
+    /**
+     * @Author zyb
+     * @Description 根据关联业务编号查询审核记录
+     * @Date 2020/7/18 9:13
+     * @Param [refId, pageNum, pageSize]
+     * @Return com.github.pagehelper.PageInfo<com.aaa.pro.model.Audit>
+     **/
+    @GetMapping("/selectAuditByRefId")
+    PageInfo<Audit> selectAuditByRefId(@RequestParam("refId") Long refId,
+                                       @RequestParam("pageNum") Integer pageNum,
+                                       @RequestParam("pageSize") Integer pageSize);
 }
